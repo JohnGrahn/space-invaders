4c30431d2d0372e41d04a6c7b0c6c0f23b2f7792
John Grahn <johngrahn@protonmail.com>
2024-06-21 16:28:04 -0400

    Fixed score and lives

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ import { Game } from "./src/components/Game.js";
 document.querySelector('#app').innerHTML = `
   <div id="game-container">
     <canvas id="game-canvas" width="800" height="600"></canvas>
-    <div id="score">Score: <span id="score-value">0</span></div>
-    <div id="lives">Lives: <span id="lives-value">3</span></div>
   </div>
 `;
 
