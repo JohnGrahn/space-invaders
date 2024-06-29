5612fd881c4e449332b707e02b21ca239399cdf5
John Grahn <johngrahn@protonmail.com>
2024-06-29 11:47:27 -0400

    Added falling enemies to start screen

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -4,7 +4,11 @@ export class StartMenu {
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
+    this.assetTypes = ['crab', 'enemy', 'squid', 'invader'];
+    this.loadedAssets = {};
     this.stars = this.createStars(200); // Create 200 stars
+    this.fallingAssets = this.createFallingAssets(40); // Create 40 falling assets (10 of each type)
+    this.loadAssets();
   }
 
   createStars(count) {
@@ -19,12 +23,48 @@ export class StartMenu {
     return stars;
   }
 
+  loadAssets() {
+    this.assetTypes.forEach(type => {
+      const img = new Image();
+      img.src = `/assets/${type}.svg`;
+      img.onload = () => {
+        this.loadedAssets[type] = img;
+      };
+    });
+  }
+
+  createFallingAssets(count) {
+    const assets = [];
+    for (let i = 0; i < count; i++) {
+      assets.push({
+        x: Math.random() * this.canvas.width,
+        y: Math.random() * this.canvas.height,
+        speed: Math.random() * 0.5 + 0.2,
+        type: this.assetTypes[i % this.assetTypes.length]
+      });
+    }
+    return assets;
+  }
+
   drawStars() {
     this.ctx.fillStyle = 'white';
     this.stars.forEach(star => {
       this.ctx.beginPath();
       this.ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
       this.ctx.fill();
+    });
+  }
+
+  drawFallingAssets() {
+    this.fallingAssets.forEach(asset => {
+      if (this.loadedAssets[asset.type]) {
+        this.ctx.drawImage(this.loadedAssets[asset.type], asset.x, asset.y, 20, 20);
+        asset.y += asset.speed;
+        if (asset.y > this.canvas.height) {
+          asset.y = -20;
+          asset.x = Math.random() * this.canvas.width;
+        }
+      }
     });
   }
 
@@ -36,10 +76,12 @@ export class StartMenu {
     // Draw stars
     this.drawStars();
 
-    // Clear area for "Space Invaders" text
-this.ctx.fillStyle = 'black';
-this.ctx.fillRect(this.canvas.width / 2 - 200, this.canvas.height / 2 - 100, 400, 100);
+    // Draw falling assets
+    this.drawFallingAssets();
 
+    // Clear area for "Space Invaders" text
+    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    this.ctx.fillRect(this.canvas.width / 2 - 200, this.canvas.height / 2 - 100, 400, 100);
 
     // Draw menu text
     this.ctx.fillStyle = 'white';
